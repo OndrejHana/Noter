42c071df772fffb10a2f57551751606ae741b1f1
OndrejHana <ondrahana001@gmail.com>
2022-12-14 18:02:21 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Editor from "./components/Editor";
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="font-bold">Hello world</h1>
+    </div>
+  )
+}
+
+export default App
